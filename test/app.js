d6a33807b1f1482fec8cee337abd58b10e87bfa9
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2014-11-25 06:41:19 +0100

    add test

---
+++ test/app.js
@@ -0,0 +1,29 @@
+(function($){
+    'use strict';
+    console.log($.fn.jquery);
+})(jq172);
+
+(function($){
+    'use strict';
+    console.log($.fn.jquery);
+})(jq183);
+
+(function($){
+    'use strict';
+    console.log($.fn.jquery);
+})(jq191);
+
+(function($){
+    'use strict';
+    console.log($.fn.jquery);
+})(jq1102);
+
+(function($){
+    'use strict';
+    console.log($.fn.jquery);
+})(jq1111);
+
+(function($){
+    'use strict';
+    console.log($.fn.jquery);
+})(jq211);
